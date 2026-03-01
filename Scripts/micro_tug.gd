503faac1b023f44bed471b29671d97f5b4382c86
Natalie Daubner <ncdaubner@gmail.com>
2026-03-01 13:27:20 -0500

    works

--- Scripts/micro_tug.gd
+++ Scripts/micro_tug.gd
@@ -1,13 +1,16 @@
 extends Node
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var area_2d: Area2D = $Area2D
-@onready var area_2d_2: Area2D = $Area2D2
+@onready var enemy: Area2D = $enemy
+@onready var you: Area2D = $you
+
 @onready var audio_manager: Node = $AudioManager
 @onready var music_manager: Node = $MusicManager
-var is_won = false
 
+var is_won = false
+var is_lost = false
 var rng = RandomNumberGenerator.new()
-var x : int = 0
+var x : int = rng.randi_range(0,4)
+
 func _ready()-> void:
 	audio_manager.load_sound("res://Assets/Sounds/SnapShot/jeopardy-themelq.mp3")
 	audio_manager.start()
@@ -15,41 +18,57 @@ func _ready()-> void:
 	
 	pass
 
+	
 func _unhandled_input(event: InputEvent) -> void:
+	
 	if Input.is_action_just_pressed("ui_accept"):
 		sprite_2d.position.x += 40
 	pass
 
 func random_tug() -> void:
-	x = rng.randi_range(0,4)
-	sprite_2d.position.x -= x
-	 
-	pass
+	if is_lost == false && is_lost == false:
+		
+		sprite_2d.position.x -= x
+
+		
+	
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("floppa")
-	if area == area_2d: #WIN condition
+	if area == you && is_won == false: #WIN condition
 		print("right")
-	
-	if is_won == false:
-		is_won = true   
-		
-		
-	#if this variable has been activated once, win achieved
+		is_won = true
 		audio_manager.load_sound("res://Assets/Sounds/SnapShot/lightskin-rizz-sin-city.mp3")
 		audio_manager.start()
 		await get_tree().create_timer(2.0).timeout
-		#release from prison
-		
- 
-	if area == area_2d_2: #LOSE condition
+	
+
+	
+	if area == enemy && is_lost == false: #loSE condition
+		is_lost = true
 		audio_manager.load_sound("res://Assets/Sounds/SnapShot/metal-pipe-clang.mp3")
 		audio_manager.start()
 		print("left")
-	pass # Replace with function body.
+		
+	 
+
+		  
+		
+		
+	#if this variable has been activated once, win achieved
+		
+		#release from prison
+		
+ 
+
 
 
 func _on_timer_timeout() -> void:
-	random_tug()
+	if is_won == false && is_lost == false:
+		random_tug()
+		
+
+		
+  	
 	#thing.visible = true
 	pass # Replace with function body.
